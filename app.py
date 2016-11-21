25116c02476d31d2c8b971936ddc545d61747dad
Neivin Mathew <neivin93@gmail.com>
2016-11-21 17:44:16 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, abort, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/aret-3750'
+
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	__tablename__="users"
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(120), unique=True)
+
+	def __init__(self, email):
+		self.email = email
+
+	def __repr__(self):
+		return '<Email: %r>' % self.email
+
+
+#routes
+@app.route('/api/user/<int:id>')
+def get_user(id):
+	user = User.query.get(id)
+	if not user:
+		abort(400)
+	return jsonify({ 'id': user.id, 'email': user.email})
+
+
+if __name__ == 'main':
+	app.debug = True
+	app.run()
