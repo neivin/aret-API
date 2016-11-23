e030a024d374276fe8caddeb3dfc2b5e55f29863
Neivin Mathew <neivin93@gmail.com>
2016-11-23 00:17:25 -0500

    delete wrong email entries, update script

--- dummy.py
+++ dummy.py
@@ -1,12 +1,15 @@
 # init farmer dummy data
+#app.db.session.query(app.Farmer).filter(app.Farmer.id>1).delete()
+
 # f = app.Farmer(name='', email=email, password_hash= pw_hash, phone=phone, region=region, age=age)
+
 import app
 from passlib.apps import custom_app_context as pwd
 
 list = ['Tennie Throckmorton','Earleen Edgemon','Brittny Bezio','Arvilla Albrecht','Leia Leake','Otilia Olsson','Georgette Galbraith','Mariam Mcdougle','Morris Meals','Demetrice Dorough','Denisha Dement','Maryetta Mcneilly','Margeret Millican','Avril Abbot','Reva Robison','Edda Emmer','Leonora Lofgren','Coleman Carbo','Greg Go','Jacques Juan']
 count = 1
 for name in list:
-	fname = name[: name.find(' ')]
+	fname = name[: name.find(' ')].lower()
 	
 	age = 45
 	region = 2
